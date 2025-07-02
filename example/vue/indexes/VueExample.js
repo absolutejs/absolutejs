ac74d6c6faa0e0a39a9adaf9e6c59dc6e5bc4548
Alex Kahn <alexkahndev@gmail.com>
2025-07-02 18:39:34 -0400

    refactor: remove VueExample generated related files

--- example/vue/indexes/VueExample.js
+++
@@ -1,5 +0,0 @@
-import { createSSRApp } from 'vue';
-import Comp from '../client/VueExample.js';
-const props = window.__INITIAL_PROPS__ ?? {};
-const app = createSSRApp(Comp, props);
-app.mount('#app');
