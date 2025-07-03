b45d956e836d4e2604e5dd47d2fd29c76284ebf2
Alex Kahn <alexkahndev@gmail.com>
2025-07-03 08:25:23 -0400

    fix: update prettier format script to include Vue files

--- example/vue/pages/VueExample.vue
+++ example/vue/pages/VueExample.vue
@@ -1,29 +1,28 @@
-  <script setup lang="ts">
-  import { ref } from 'vue'
-  
-  const props = defineProps<{
-    test: number
-  }>()
-  
-  const count = ref(props.test)
-  </script>
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const props = defineProps<{
+	test: number;
+}>();
+
+const count = ref(props.test);
+</script>
 
 <template>
-  <div class="counter">
-    <p>Current count: {{ count }}</p>
-    <button @click="count++">Increment</button>
-  </div>
+	<div class="counter">
+		<p>Current count: {{ count }}</p>
+		<button @click="count++">Increment</button>
+	</div>
 </template>
-
 
 <style scoped>
 .counter {
-  display: flex;
-  align-items: center;
-  gap: 1rem;
+	display: flex;
+	align-items: center;
+	gap: 1rem;
 }
 button {
-  padding: 0.5rem 1rem;
-  font-size: 1rem;
+	padding: 0.5rem 1rem;
+	font-size: 1rem;
 }
 </style>
