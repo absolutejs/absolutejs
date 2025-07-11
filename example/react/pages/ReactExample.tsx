c07370b19d693e30d335cae40c6ee1d044bed0ba
Alex Kahn <alexkahndev@gmail.com>
2025-07-11 05:18:06 -0400

    refactor: extract Dropdown component for improved modularity and reusability

--- example/react/pages/ReactExample.tsx
+++ example/react/pages/ReactExample.tsx
@@ -1,36 +1,18 @@
-import { useState } from 'react';
 import { App } from '../components/App';
+import { Dropdown } from '../components/Dropdown';
 import { Head } from '../components/Head';
 
 type ReactExampleProps = { initialCount: number; cssPath: string };
 
-export const ReactExample = ({ initialCount, cssPath }: ReactExampleProps) => {
-	const [isOpen, setIsOpen] = useState(false);
-
-	return (
-		<html>
-			<Head cssPath={cssPath} />
-			<body>
-				<header>
-					<a href="/">AbsoluteJS</a>
-					<details
-						onPointerEnter={() => setIsOpen(true)}
-						onPointerLeave={() => setIsOpen(false)}
-						open={isOpen}
-					>
-						<summary>Pages</summary>
-						<nav>
-							<a href="/html">HTML</a>
-							<a href="/react">React</a>
-							<a href="/htmx">HTMX</a>
-							<a href="/svelte">Svelte</a>
-							<a href="/vue">Vue</a>
-							<a href="/angular">Angular</a>
-						</nav>
-					</details>
-				</header>
-				<App initialCount={initialCount} />
-			</body>
-		</html>
-	);
-};
+export const ReactExample = ({ initialCount, cssPath }: ReactExampleProps) => (
+	<html>
+		<Head cssPath={cssPath} />
+		<body>
+			<header>
+				<a href="/">AbsoluteJS</a>
+				<Dropdown />
+			</header>
+			<App initialCount={initialCount} />
+		</body>
+	</html>
+);
