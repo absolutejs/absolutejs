7ab8b09d7579ff846dc32d5c6ef1267fa5c78b3b
Alex Kahn <alexkahndev@gmail.com>
2025-07-11 04:08:42 -0400

    refactor: enhance header navigation with details element and improve styles for dropdown

--- example/react/pages/ReactExample.tsx
+++ example/react/pages/ReactExample.tsx
@@ -1,24 +1,36 @@
+import { useState } from 'react';
 import { App } from '../components/App';
 import { Head } from '../components/Head';
 
 type ReactExampleProps = { initialCount: number; cssPath: string };
 
-export const ReactExample = ({ initialCount, cssPath }: ReactExampleProps) => (
-	<html>
-		<Head cssPath={cssPath} />
-		<body>
-			<header>
-				<p>AbsoluteJS</p>
-				<nav>
-					<a href="/">HTML</a>
-					<a href="/react">React</a>
-					<a href="/svelte">Svelte</a>
-					<a href="/vue">Vue</a>
-					<a href="/angular">Angular</a>
-					<a href="/htmx">HTMX</a>
-				</nav>
-			</header>
-			<App initialCount={initialCount} />
-		</body>
-	</html>
-);
+export const ReactExample = ({ initialCount, cssPath }: ReactExampleProps) => {
+	const [isOpen, setIsOpen] = useState(false);
+
+	return (
+		<html>
+			<Head cssPath={cssPath} />
+			<body>
+				<header>
+					<a href="/">AbsoluteJS</a>
+					<details
+						onPointerEnter={() => setIsOpen(true)}
+						onPointerLeave={() => setIsOpen(false)}
+						open={isOpen}
+					>
+						<summary>Pages</summary>
+						<nav>
+							<a href="/html">HTML</a>
+							<a href="/react">React</a>
+							<a href="/htmx">HTMX</a>
+							<a href="/svelte">Svelte</a>
+							<a href="/vue">Vue</a>
+							<a href="/angular">Angular</a>
+						</nav>
+					</details>
+				</header>
+				<App initialCount={initialCount} />
+			</body>
+		</html>
+	);
+};
