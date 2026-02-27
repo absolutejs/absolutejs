b06e0693fc43dfac49ebe8296693fce23eb1dc94
Alex Kahn <alexkahndev@gmail.com>
2026-02-27 01:45:25 -0500

    fix: update version to 0.17.9 and simplify asset lookup function

--- src/core/lookup.ts
+++ src/core/lookup.ts
@@ -1,15 +1,5 @@
-const isWrapped = (
-	source: Record<string, string> | { manifest: Record<string, string> }
-): source is { manifest: Record<string, string> } =>
-	'manifest' in source &&
-	typeof source.manifest === 'object' &&
-	!Array.isArray(source.manifest);
-
-export const asset = (
-	source: Record<string, string> | { manifest: Record<string, string> },
-	name: string
-) => {
-	const assetPath = isWrapped(source) ? source.manifest[name] : source[name];
+export const asset = (source: Record<string, string>, name: string) => {
+	const assetPath = source[name];
 
 	if (assetPath === undefined) {
 		throw new Error(`Asset "${name}" not found in manifest.`);
